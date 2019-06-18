132acc337b4b2b1768eaf1c43e3e6a9c7ae82a20
Ильнур <ilnurgal1994@gmail.com>
2019-06-18 15:26:56 +0300

    Added qustion and answer classes

---
+++ TestingApplication/Models/Question.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestingApplication.Models
+{
+    public class Question
+    {
+        public int QuestionID { get; set; }
+        public string QuestionText { get; set; }
+    }
+}
